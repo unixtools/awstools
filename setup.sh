b9d5b8201395013919ba65305c7dd553182916e2
Nathan Neulinger <nneul@neulinger.org>
2013-05-27 16:19:07 +0000

    updates

--- setup.sh
+++ setup.sh
@@ -2,13 +2,13 @@
 
 # this file should be sourced with . /local/awstools/setup.sh
 
-export AWS_ELB_HOME=/local/awstools/ElasticLoadBalancing-1.0.15.1
+export AWS_ELB_HOME=/local/awstools/ElasticLoadBalancing-1.0.17.0
 export AWS_CREDENTIAL_FILE=$HOME/.awsiam
 export EC2_PRIVATE_KEY=$HOME/.aws-pk.pem
 export EC2_CERT=$HOME/.aws-cert.pem
 
-export EC2_HOME=/local/awstools/ec2-api-tools-1.5.2.5
+export EC2_HOME=/local/awstools/ec2-api-tools-1.6.7.3
 
-export AWS_IAM_HOME=/local/awstools/IAMCli-1.3.0
+export AWS_IAM_HOME=/local/awstools/IAMCli-1.5.0
 
 export PATH=$AWS_ELB_HOME/bin:$EC2_HOME/bin:$AWS_IAM_HOME/bin:$PATH
